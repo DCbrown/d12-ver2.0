f75b1b763da1c3701d9c7ffc8cc20a0d98ba7f3e
dcbrown <browncdonovan@gmail.com>
2022-04-03 16:21:43 -0400

    add dice roll log logic

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+
+import React, { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Modal, Portal, Text, Button, Provider } from 'react-native-paper';
@@ -13,14 +14,47 @@ import { D8light } from './img/D8light.svg';
 
 export default function App() {
   const [visible, setVisible] = React.useState(false);
-  const [counter, setCounter] = React.useState(0);
+  const [modifier, setModifier] = React.useState(0);
+  const [dR, setDR] = useState(0);
+  const [logs, setLogs] = useState([])
 
   const showModal = () => setVisible(true);
   const hideModal = () => { 
     setVisible(false); 
-    setCounter(0);
+    setModifier(0);
   }
-  const containerStyle = {backgroundColor: 'white', padding: 20};
+
+  const roll = (d) => {
+    setTimeout(()=> {
+      setDR(Math.floor(Math.random() * d) + 1);
+      rollingDice();
+    }, 500)
+  }
+
+  const add = () => {
+    setModifier((prev) => {
+      return prev + 1;
+    });
+  }
+
+  const subtract = () => {
+    if (modifier != 0) {
+      setModifier((prev) => {
+        return prev - 1;
+      });
+    }
+  }
+
+  const rollingDice = () => {
+    let log = `${dR} + ${modifier} = ${dR + modifier}`
+
+
+    setLogs(logs.concat(log))
+    console.log("-")
+    console.log(logs)
+  }
+
+  const containerStyle = { backgroundColor: 'white', padding: 20 };
   const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
 
   return (
@@ -40,52 +74,56 @@ export default function App() {
               height="20%"
               uri="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/debian.svg"
           />
+
+          <List.Item
+              key={ index }
+              titleStyle = {{textAlign: 'right' }}
+              title={`${log.name}`}
+              left={props => <List.Icon {...props} icon="folder" />}
+            />
           */}  
-            <Button style={styles.button} icon="camera" mode="contained" onPress={() => hideModal()}></Button>
-            <Button style={styles.button} icon="camera" mode="contained" onPress={() => console.log('Pressed')}></Button>
-            <Button style={styles.button} icon="camera" mode="contained" onPress={() => console.log('Pressed')}></Button>
+            <Button style={styles.button} icon="camera" mode="contained" onPress={() => roll(4)}></Button>
+            <Button style={styles.button} icon="camera" mode="contained" onPress={() => roll(6)}></Button>
+            <Button style={styles.button} icon="camera" mode="contained" onPress={() => roll(8)}></Button>
           </View>
           <View style={styles.container} >
-            <Button style={styles.button} icon="camera" mode="contained" onPress={() => console.log('Pressed')}></Button>
-            <Button style={styles.button} icon="camera" mode="contained" onPress={() => console.log('Pressed')}></Button>
-            <Button style={styles.button} icon="camera" mode="contained" onPress={() => console.log('Pressed')}></Button>
+            <Button style={styles.button} icon="camera" mode="contained" onPress={() => roll(10)}></Button>
+            <Button style={styles.button} icon="camera" mode="contained" onPress={() => roll(12)}></Button>
+            <Button style={styles.button} icon="camera" mode="contained" onPress={() => roll(20)}></Button>
           </View>
           <View style={styles.container} >
-            <Button style={styles.button} icon="camera" mode="contained" onPress={() => console.log('Pressed')}></Button>
+            <Button style={styles.button} icon="camera" mode="contained" onPress={() => add()}></Button>
             <TextInput
               style={styles.input}
-              value={counter}
+              value={modifier.toString()}
               keyboardType = 'numeric'
-              onChangeText={counter => setCounter(counter)}
+              onChangeText={modifier => setModifier(modifier)}
             />
-            <Button style={styles.button} icon="camera" mode="contained" onPress={() => console.log('Pressed')}></Button>
+            <Button style={styles.button} icon="camera" mode="contained" onPress={() => subtract()}></Button>
           </View>
         </Modal>
-       
       </Portal>
+      
       <Button style={{marginTop: 105}} onPress={showModal}>
         Show
       </Button>
+      <Text>{modifier}</Text>
+      <Text>{dR}</Text>
       <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-      <List.Item
-        titleStyle = {{textAlign: 'right' }}
-        title="7 + 2 = 9"
-        left={props => <List.Icon {...props} icon="folder" />}
-      />
-      <Divider />
-      <List.Item
-        titleStyle = {{textAlign: 'right' }}
-        title="7 + 2 = 9"
-        left={props => <List.Icon {...props} icon="folder" />}
-      />
-       <Divider />
-      <List.Item
-        titleStyle = {{textAlign: 'right' }}
-        title="7 + 2 = 9"
-        left={props => <List.Icon {...props} icon="folder" />}
-      />
-           </ScrollView>
+        <ScrollView style={styles.scrollView}>
+           <View>
+              {logs && logs.length ? (
+                logs.map((log, index) => {
+                  return<List.Item key={ index } titleStyle = {{textAlign: 'right' }} title={`${log}`} left={props => <List.Icon {...props} icon="folder" />}
+              />
+              })) : null }
+                
+              
+
+              <Divider />
+            </View>  
+         
+      </ScrollView>
     </SafeAreaView>
     </Provider>
   );
